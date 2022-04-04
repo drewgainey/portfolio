056142c87a26c65b8e61581c58cb7979d05d8847
Drew Gainey <dgainey@mallproperties.com>
2022-04-04 11:18:58 -0400

    updates to projects and contacts section

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const body = document.body;
+//projects elements
 const projects = document.getElementById("projects");
 const project1 = document.getElementById("project1");
 const project1Img = document.getElementById("project1Img");
+const project1Link = document.getElementById("project1Link")
 const project1Details = document.getElementById("project1Details");
 const project2 = document.getElementById("project2");
 const project3 = document.getElementById("project3");
 
-project1.onclick = () => {
-  if (project1Img.className === "projectImage") {
-    project1Img.classList.replace("projectImage", "projectImageHidden");
-    project1Details.classList.replace("projectDetails", "projectDetailsActive");
-  } else {
-    project1Img.classList.replace("projectImageHidden", "projectImage");
-    project1Details.classList.replace("projectDetailsActive", "projectDetails");
-  }
-};
+//contact elements
+const phoneIcon = document.getElementById("phoneIcon");
+const phoneInfo = document.getElementById("phoneInfo")
 
+//projects eventListeners
 project1Img.addEventListener("mouseover", () => {
   project1Img.classList.replace("projectImage", "projectImageHidden");
   project1Details.classList.replace("projectDetails", "projectDetailsActive");
@@ -25,3 +22,15 @@ project1.addEventListener("mouseleave", () => {
   project1Img.classList.replace("projectImageHidden", "projectImage");
   project1Details.classList.replace("projectDetailsActive", "projectDetails");
 });
+
+project1Link.addEventListener("click", () => {
+  console.log("clicked");
+});
+
+//contacts eventListeners
+phoneIcon.addEventListener("mouseover", () => {
+  phoneInfo.classList.replace("hiddenContact", "activeContact");
+})
+phoneIcon.addEventListener("mouseleave", () => {
+  phoneInfo.classList.replace("activeContact", "hiddenContact");
+})
