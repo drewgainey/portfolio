d45b08c1858fd6a8f6a123a0ad01451c97d447c3
Drew Gainey <dgainey@mallproperties.com>
2022-04-06 17:32:18 -0400

    added details to projects

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const body = document.body;
 const projects = document.getElementById("projects");
 const project1 = document.getElementById("project1");
 const project1Img = document.getElementById("project1Img");
-const project1Link = document.getElementById("project1Link")
+const project1Link = document.getElementById("project1Link");
 const project1Details = document.getElementById("project1Details");
 const project2 = document.getElementById("project2");
 const project3 = document.getElementById("project3");
 
 //contact elements
 const phoneIcon = document.getElementById("phoneIcon");
-const phoneInfo = document.getElementById("phoneInfo")
+const phoneInfo = document.getElementById("phoneInfo");
 
-//projects eventListeners
+// projects eventListeners
 project1Img.addEventListener("mouseover", () => {
   project1Img.classList.replace("projectImage", "projectImageHidden");
   project1Details.classList.replace("projectDetails", "projectDetailsActive");
@@ -23,14 +23,31 @@ project1.addEventListener("mouseleave", () => {
   project1Details.classList.replace("projectDetailsActive", "projectDetails");
 });
 
-project1Link.addEventListener("click", () => {
-  console.log("clicked");
+project2Img.addEventListener("mouseover", () => {
+  project2Img.classList.replace("projectImage", "projectImageHidden");
+  project2Details.classList.replace("projectDetails", "projectDetailsActive");
 });
+
+project2.addEventListener("mouseleave", () => {
+  project2Img.classList.replace("projectImageHidden", "projectImage");
+  project2Details.classList.replace("projectDetailsActive", "projectDetails");
+});
+
+project3Img.addEventListener("mouseover", () => {
+  project3Img.classList.replace("projectImage", "projectImageHidden");
+  project3Details.classList.replace("projectDetails", "projectDetailsActive");
+});
+
+project3.addEventListener("mouseleave", () => {
+  project3Img.classList.replace("projectImageHidden", "projectImage");
+  project3Details.classList.replace("projectDetailsActive", "projectDetails");
+});
+
 
 //contacts eventListeners
 phoneIcon.addEventListener("mouseover", () => {
   phoneInfo.classList.replace("hiddenContact", "activeContact");
-})
+});
 phoneIcon.addEventListener("mouseleave", () => {
   phoneInfo.classList.replace("activeContact", "hiddenContact");
-})
+});
